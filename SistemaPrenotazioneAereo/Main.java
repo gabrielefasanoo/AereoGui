1921e45ee9d6d5487e3ce39cf874009e00e700a1
gabrielefasanoo <115032812+gabrielefasanoo@users.noreply.github.com>
2025-02-22 18:18:01 +0100

    Delete SistemaPrenotazioneAereo directory

--- SistemaPrenotazioneAereo/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Volo volo1 = new Volo("AZ2024", 36, "Roma", "Milano");
-        AirplaneGUI gui = new AirplaneGUI(volo1);
-        gui.setVisible(true);
-    }
-}
